7b25f5c55b379226671a939b06441e0047eaae40
Zuckuu <entbit12@gmail.com>
2022-11-11 11:48:15 -0600

    noce

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ todoMain();
 function todoMain() {
   const DEFAULT_OPTION = "Choose the category";
 
-  let inputElem, inputElem2, button, selectElem;
+  let inputElem,
+    inputElem2,
+    button,
+    selectElem,
+    todoList = [];
 
   getElements();
   addListeners();
+  load();
+  renderRows();
 
   function getElements() {
     inputElem = document.getElementsByTagName("input")[0];
@@ -27,48 +33,13 @@ function todoMain() {
     let inputValue2 = inputElem2.value;
     inputElem2.value = "";
 
-    let table = document.getElementById("todoTable");
+    renderRow(inputValue, inputValue2);
 
-    let trElem = document.createElement("tr");
-    table.appendChild(trElem);
+    todoList.push(inputValue);
 
-    //checkbox cell
-    let checkboxElem = document.createElement("input");
-    checkboxElem.type = "checkbox";
-    checkboxElem.addEventListener("click", done, false);
-    let tdElem1 = document.createElement("td");
-    tdElem1.appendChild(checkboxElem);
-    trElem.appendChild(tdElem1);
-
-    //Assignments cell
-    let tdElem2 = document.createElement("td");
-    tdElem2.innerText = inputValue;
-    trElem.appendChild(tdElem2);
-
-    //Category cell
-    let tdElem3 = document.createElement("td");
-    tdElem3.innerText = inputValue2;
-    trElem.appendChild(tdElem3);
-
-    //Detele cell
-    let spanElem = document.createElement("i");
-    spanElem.innerText = "";
-    spanElem.className = "fa-solid fa-trash-can";
-    spanElem.addEventListener("click", deleteItem, false);
-    let tdElem4 = document.createElement("td");
-    tdElem4.appendChild(spanElem);
-    trElem.appendChild(tdElem4);
+    save();
 
     updateSelectOptions();
-
-    function deleteItem() {
-      trElem.remove();
-      updateSelectOptions();
-    }
-
-    function done() {
-      trElem.classList.toggle("strike");
-    }
   }
 
   function filterEntries() {
@@ -128,6 +99,68 @@ function todoMain() {
       newOptionElem.value = option;
       newOptionElem.innerText = option;
       selectElem.appendChild(newOptionElem);
+    }
+  }
+
+  function save() {
+    let stringified = JSON.stringify(todoList);
+    localStorage.setItem("todoList", stringified);
+  }
+
+  function load() {
+    let retrieved = localStorage.getItem("todoList");
+    todoList = JSON.parse(retrieved)
+    if (todoList == null) {
+      todoList = [];
+    }
+  }
+
+  function renderRows() {
+    todoList.forEach((todo) => {
+      renderRow(todo, null);
+    });
+  }
+
+  function renderRow(inputValue, inputValue2) {
+    let table = document.getElementById("todoTable");
+
+    let trElem = document.createElement("tr");
+    table.appendChild(trElem);
+
+    //checkbox cell
+    let checkboxElem = document.createElement("input");
+    checkboxElem.type = "checkbox";
+    checkboxElem.addEventListener("click", done, false);
+    let tdElem1 = document.createElement("td");
+    tdElem1.appendChild(checkboxElem);
+    trElem.appendChild(tdElem1);
+
+    //Assignments cell
+    let tdElem2 = document.createElement("td");
+    tdElem2.innerText = inputValue;
+    trElem.appendChild(tdElem2);
+
+    //Category cell
+    let tdElem3 = document.createElement("td");
+    tdElem3.innerText = inputValue2;
+    trElem.appendChild(tdElem3);
+
+    //Detele cell
+    let spanElem = document.createElement("i");
+    spanElem.innerText = "";
+    spanElem.className = "fa-solid fa-trash-can";
+    spanElem.addEventListener("click", deleteItem, false);
+    let tdElem4 = document.createElement("td");
+    tdElem4.appendChild(spanElem);
+    trElem.appendChild(tdElem4);
+
+    function deleteItem() {
+      trElem.remove();
+      updateSelectOptions();
+    }
+
+    function done() {
+      trElem.classList.toggle("strike");
     }
   }
 }
